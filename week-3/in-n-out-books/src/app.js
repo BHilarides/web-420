cfb27e9622b046f5c5b22387b9b6b29d6a3f98dc
Ben Hilarides <ben.hilarides26@gmail.com>
2025-09-01 16:08:40 -0400

    meow

--- week-3/in-n-out-books/src/app.js
+++ week-3/in-n-out-books/src/app.js
@@ -77,8 +77,8 @@ app.get("/", (req, res) => {
         </section>
         <section id="hours" class="hours">
           <h2>Hours of Operation</h2>
-          <p>Monday – Friday: 9 AM – 8 PM</p>
-          <p>Saturday: 10 AM – 6 PM</p>
+          <p>Monday - Friday: 9 AM - 8 PM</p>
+          <p>Saturday: 10 AM - 6 PM</p>
           <p>Sunday: Closed</p>
         </section>
         <section id="contact" class="contact">
